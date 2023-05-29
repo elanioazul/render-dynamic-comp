9741eef432b02ae3aa3cc777bfd3a1832d72dbfe
hugo <hugo.herrador@gmail.com>
2023-05-29 10:02:23 +0200

    Create comp dymamincally with viewContainerRef

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ComponentRef, ViewChild, ViewContainerRef } from '@angular/core';
+import { WidgetComponent } from "./@shared/components/widgets/widget/widget.component";
+import { statisticData } from "./const-data/statistics.data";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dynamic-components';
+
+  optionsByType = (type: string) => {
+    switch (type) {
+      case 'fb':
+        return {
+          component: () => import('./@shared/components/widgets/widget/widget.component').then(m => m.WidgetComponent),
+          inputs: statisticData.find(item => item['id'] === type)!
+        }
+      case 'tw':
+        return {
+          component: () => import('./@shared/components/widgets/widget/widget.component').then(m => m.WidgetComponent),
+          inputs: statisticData.find(item => item['id'] === type)!
+        }
+      case 'ig':
+        return {
+          component: () => import('./@shared/components/widgets/widget/widget.component').then(m => m.WidgetComponent),
+          inputs: statisticData.find(item => item['id'] === type)!
+        }
+      default:
+        return {
+          component: () => import('./@shared/components/widgets/widget/widget.component').then(m => m.WidgetComponent),
+          inputs: statisticData.find(item => item['id'] === type)!
+        }
+    }
+  }
+
+  @ViewChild('container', {
+    static: true,
+    read: ViewContainerRef
+  }) container!: ViewContainerRef;
+  buttons: any[] = [
+    {
+      label: 'Create Widget Facebook',
+      action: () => this.createDynamicWidget('fb')
+    },
+    {
+      label: 'Create Widget Twitter',
+      action: () => this.createDynamicWidget('tw')
+    },
+    {
+      label: 'Create Widget Instagram',
+      action: () => this.createDynamicWidget('ig')
+    }
+  ]
+
+  async createDynamicWidget(type: string) {
+
+    this.container.clear();
+    const {component, inputs} = this.optionsByType(type);
+
+    const componentInstance = await component();
+    const componentRef: ComponentRef<WidgetComponent> = this.container.createComponent(componentInstance);
+    componentRef.instance.configOptions = inputs;
+  }
 }
